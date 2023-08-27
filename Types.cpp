190c25833afc4746c6caf81ee578b219ae709221
or-nob <ishraqarnob@gmail.com>
2023-08-27 23:41:51 +0600

    clean up and restructure

---
+++ Types.cpp
@@ -0,0 +1,180 @@
+#include "Types.h"
+
+#include "Env.h"
+#include "Eval.h"
+
+Number& Number::operator+=(const Number& o) {
+    *this = operator+(o);
+    return *this;
+}
+
+Number& Number::operator-=(const Number& o) {
+    *this = operator-(o);
+    return *this;
+}
+
+Number& Number::operator*=(const Number& o) {
+    *this = operator*(o);
+    return *this;
+}
+
+Number& Number::operator/=(const Number& o) {
+    *this = operator/(o);
+    return *this;
+}
+
+Number Number::operator+(const Number& o) {
+    Number n;
+    n._t = (_t == 0 && o._t == 0) ? 0 : 1;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    if (n._t == 0)
+        n._v._i = f + s;
+    else
+        n._v._f = f + s;
+
+    return n;
+}
+
+Number Number::operator-(const Number& o) {
+    Number n;
+    n._t = (_t == 0 && o._t == 0) ? 0 : 1;
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    if (n._t == 0)
+        n._v._i = f - s;
+    else
+        n._v._f = f - s;
+
+    return n;
+}
+
+Number Number::operator*(const Number& o) {
+    Number n;
+    n._t = (_t == 0 && o._t == 0) ? 0 : 1;
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    if (n._t == 0)
+        n._v._i = f * s;
+    else
+        n._v._f = f * s;
+
+    return n;
+}
+
+Number Number::operator/(const Number& o) {
+    Number n;
+    n._t = (_t == 0 && o._t == 0) ? 0 : 1;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    if (n._t == 0)
+        n._v._i = f / s;
+    else
+        n._v._f = f / s;
+
+    return n;
+}
+
+Number Number::operator>(const Number& o) {
+    Number n;
+    n._t = 0;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    n._v._i = f > s;
+    return n;
+}
+
+Number Number::operator>=(const Number& o) {
+    Number n;
+    n._t = 0;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    n._v._i = f >= s;
+    return n;
+}
+
+Number Number::operator<(const Number& o) {
+    Number n;
+    n._t = 0;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    n._v._i = f < s;
+    return n;
+}
+
+Number Number::operator<=(const Number& o) {
+    Number n;
+    n._t = 0;
+
+    float f = (_t == 0) ? _v._i : _v._f;
+    float s = (o._t == 0) ? o._v._i : o._v._f;
+
+    n._v._i = f <= s;
+    return n;
+}
+
+Number Number::operator==(const Number& o) {
+    Number n;
+    n._t = 0;
+    n._v._i = _t == o._t && _v._i == o._v._i && _v._f == o._v._f;
+    return n;
+}
+
+Number Number::abs(const Number& o) {
+    Number n;
+    n._t = o._t;
+    if (o._t == 0)
+        n._v._i = fabs(o._v._i);
+    else
+        n._v._f = fabs(o._v._f);
+    return n;
+}
+
+Exp::Exp(const Exp::ExpType& v, std::uint8_t t) : _v(v), _t(t) {}
+
+Exp::Exp(const std::vector<std::string>& params, const Exp& body, Env& env) {
+    _v = {._l = {std::function<Exp(std::vector<Exp>)>{
+              [this, params, body, &env](std::vector<Exp> expList) -> Exp {
+                  Env e{params, expList, env};
+                  return Eval::eval(body, e);
+              }}}};
+    _t = 1;
+}
+
+Exp& Exp::operator=(const Exp& o) {
+    _v = o._v;
+    _t = o._t;
+    return *this;
+}
+
+void Exp::print(Exp e) {
+    if (e._t == 0) {
+        Atom a = e._v._a;
+        if (a._t == 0) {
+            std::cout << a._v._s << '\n';
+        } else {
+            Number n = a._v._n;
+            std::cout << (n._t == 0 ? n._v._i : n._v._f) << '\n';
+        }
+    } else {
+        for (auto& exp : e._v._l) {
+            try {
+                print(std::any_cast<Exp>(exp));
+            } catch (std::bad_any_cast exp) {
+                std::cerr << exp.what();
+            }
+        }
+    }
+}
--- Types.h
+++ Types.h
@@ -6,8 +6,7 @@
 #include <iostream>
 #include <vector>
 
-#include "Env.h"
-#include "Eval.h"
+class Env;
 
 struct Number {
     typedef struct {
@@ -18,144 +17,23 @@ struct Number {
     NumberType _v;
     std::uint8_t _t;
 
-    Number& operator+=(const Number& o) {
-        *this = operator+(o);
-        return *this;
-    }
+    Number& operator+=(const Number& o);
+    Number& operator-=(const Number& o);
+    Number& operator*=(const Number& o);
+    Number& operator/=(const Number& o);
 
-    Number& operator-=(const Number& o) {
-        *this = operator-(o);
-        return *this;
-    }
+    Number operator+(const Number& o);
+    Number operator-(const Number& o);
+    Number operator*(const Number& o);
+    Number operator/(const Number& o);
 
-    Number& operator*=(const Number& o) {
-        *this = operator*(o);
-        return *this;
-    }
+    Number operator>(const Number& o);
+    Number operator>=(const Number& o);
+    Number operator<(const Number& o);
+    Number operator<=(const Number& o);
+    Number operator==(const Number& o);
 
-    Number& operator/=(const Number& o) {
-        *this = operator/(o);
-        return *this;
-    }
-
-    Number operator+(const Number& o) {
-        Number n;
-        n._t = (_t == 0 && o._t == 0) ? 0 : 1;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        if (n._t == 0)
-            n._v._i = f + s;
-        else
-            n._v._f = f + s;
-
-        return n;
-    }
-
-    Number operator-(const Number& o) {
-        Number n;
-        n._t = (_t == 0 && o._t == 0) ? 0 : 1;
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        if (n._t == 0)
-            n._v._i = f - s;
-        else
-            n._v._f = f - s;
-
-        return n;
-    }
-
-    Number operator*(const Number& o) {
-        Number n;
-        n._t = (_t == 0 && o._t == 0) ? 0 : 1;
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        if (n._t == 0)
-            n._v._i = f * s;
-        else
-            n._v._f = f * s;
-
-        return n;
-    }
-
-    Number operator/(const Number& o) {
-        Number n;
-        n._t = (_t == 0 && o._t == 0) ? 0 : 1;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        if (n._t == 0)
-            n._v._i = f / s;
-        else
-            n._v._f = f / s;
-
-        return n;
-    }
-
-    Number operator>(const Number& o) {
-        Number n;
-        n._t = 0;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        n._v._i = f > s;
-        return n;
-    }
-
-    Number operator>=(const Number& o) {
-        Number n;
-        n._t = 0;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        n._v._i = f >= s;
-        return n;
-    }
-
-    Number operator<(const Number& o) {
-        Number n;
-        n._t = 0;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        n._v._i = f < s;
-        return n;
-    }
-
-    Number operator<=(const Number& o) {
-        Number n;
-        n._t = 0;
-
-        float f = (_t == 0) ? _v._i : _v._f;
-        float s = (o._t == 0) ? o._v._i : o._v._f;
-
-        n._v._i = f <= s;
-        return n;
-    }
-
-    Number operator==(const Number& o) {
-        Number n;
-        n._t = 0;
-        n._v._i = _t == o._t && _v._i == o._v._i && _v._f == o._v._f;
-        return n;
-    }
-
-    static Number abs(const Number& o) {
-        Number n;
-        n._t = o._t;
-        if (o._t == 0)
-            n._v._i = fabs(o._v._i);
-        else
-            n._v._f = fabs(o._v._f);
-        return n;
-    }
+    static Number abs(const Number& o);
 };
 
 struct Atom {
@@ -167,8 +45,6 @@ struct Atom {
     std::uint8_t _t;
 };
 
-struct Procedure;
-
 struct Exp {
     typedef struct {
         Atom _a;
@@ -177,45 +53,13 @@ struct Exp {
     ExpType _v;
     std::uint8_t _t;
 
-    Exp& operator=(const Exp& o) {
-        _v = o._v;
-        _t = o._t;
-        return *this;
-    }
-
     Exp() = default;
+    Exp(const ExpType& v, std::uint8_t t);
+    Exp(const std::vector<std::string>& params, const Exp& body, Env& env);
 
-    Exp(const ExpType& v, std::uint8_t t) : _v(v), _t(t) {}
+    Exp& operator=(const Exp& o);
 
-    Exp(const std::vector<std::string>& params, const Exp& body, Env& env) {
-        _v = {._l = {std::function<Exp(std::vector<Exp>)>{
-                  [this, params, body, &env](std::vector<Exp> expList) -> Exp {
-                      Env e{params, expList, env};
-                      return Eval::eval(body, e);
-                  }}}};
-        _t = 1;
-    }
-
-    static void print(Exp e) {
-        if (e._t == 0) {
-            Atom a = e._v._a;
-            if (a._t == 0) {
-                std::cout << a._v._s << '\n';
-            } else {
-                Number n = a._v._n;
-                std::cout << (n._t == 0 ? n._v._i : n._v._f) << '\n';
-            }
-        } else {
-            for (auto& exp : e._v._l) {
-                // std::cout << exp.type().name() << "\n";
-                try {
-                    print(std::any_cast<Exp>(exp));
-                } catch (std::bad_any_cast exp) {
-                    std::cerr << exp.what();
-                }
-            }
-        }
-    }
+    static void print(Exp e);
 };
 
 enum class Types { SYMBOL, NUMBER, ATOM, LIST, EXP, ENV };
